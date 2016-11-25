9c389fd1f7c54eb9797b32bb22c5282944088795
David R. Poindexter III <drpoindexter@gmail.com>
2016-11-25 14:10:45 -0500

    Fixes dummy header data in real app

--- src/app/App.js
+++ src/app/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Header headerText="mPg app bob" />
+          <Header headerText="my mpg app" />
         <p className="App-intro">
           Simple fuel economy tracking.
         </p>
